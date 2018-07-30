85f0b74a36d203ee30240a6d1d82593df3ab60dd
Eduarda Scharnhorst <eduardascharnhorst@gmail.com>
2018-07-29 23:06:46 -0300

    MenuInfo complete fot desktop

--- src/components/movieCast/MovieCast.jsx
+++ src/components/movieCast/MovieCast.jsx
@@ -1,18 +1,31 @@
 import React, { Component } from 'react'
 import { Fetch } from 'react-data-fetching'
+import './movieCast.css'
 
 class MovieCast extends React.Component {
-    render() {
-        return (
-            <Fetch url="https://sample-api-78c77.firebaseio.com/tv-shows/SHOW123.json">
-                {({ data }) =>
-                    <div className="movie-cast">
-                        <p>Content Cast</p>
-                    </div>
-                }
-            </Fetch>
-        )
+
+  renderCast = ({data}) => {
+    let contentCast = []
+    for (let index = 0; index < data.Cast.length; index++) {
+      contentCast.push(
+        <div className="movie-cast__box">
+          <p key={data.Cast[index].ID} className="movie-cast__actor">{data.Cast[index].Name}</p>
+        </div>
+      )
     }
+    return contentCast
+  }
+render() {
+  return (
+    <div className="movie-cast">
+      <Fetch
+        url="https://sample-api-78c77.firebaseio.com/tv-shows/SHOW123.json"
+        render={this.renderCast}
+      >
+      </Fetch>
+    </div>
+    )
+  }
 }
 
 export default MovieCast
